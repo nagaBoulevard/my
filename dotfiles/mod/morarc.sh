43d2df7543f9df6423c8febad4092528dc5ef6ca
gilo <gilotto.is@gmail.com>
2017-05-16 23:18:24 +0000

    :rocket: basic config for mora

---
+++ dotfiles/mod/morarc.sh
@@ -0,0 +1,24 @@
+# PROMPT {{{
+
+#original PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\h\[\033[00m\] \[\033[01;34m\]\w\[\033[00m\]\$ '
+BOLD='\[\033[1m\]'
+BLACK='\[\033[30m\]'
+RED='\[\033[31m\]'
+GREEN='\[\033[32m\]'
+YELLOW='\[\033[33m\]'
+BLUE='\[\033[34m\]'
+PURPLE='\[\033[35m\]'
+CYAN='\[\033[36m\]'
+WHITE='\[\033[37m\]'
+DEFAULT='\[\033[39m\]'
+END='\[\033[0m\]'
+
+
+PS1="\n\
+${BOLD}\
+${CYAN}\w${BLACK} \$(git_branch)\n\
+${CYAN}~>$END "
+
+# }}}
+
+
