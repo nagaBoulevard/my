c6d0f545d3426763b7cc8b87ab57bae5711443b4
sgilotto <sgilotto@login1.nfs>
2019-12-09 10:34:12 +0100

    autoconfig

---
+++ startup.sh
@@ -0,0 +1,8 @@
+# configuration file
+
+
+# dotfiles configuration
+ln -s ./dotfiles/bashrc     $HOME/.bashrc
+ln -s ./dotfiles/vimrc      $HOME/.vimrc
+ln -s ./dotfiles/tmux.conf  $HOME/.tmux.conf
+ln -s ./dotfiles/vim.d      $HOME/.vim/
