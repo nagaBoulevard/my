7073fb547e4c64f380c3103d36e8cfefc0d53e7b
sgilotto <gilotto.stefano@hsr.it>
2019-12-09 10:42:17 +0100

    revision

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@
 
 
 # dotfiles configuration
-ln -s ./dotfiles/bashrc     $HOME/.bashrc
-ln -s ./dotfiles/vimrc      $HOME/.vimrc
-ln -s ./dotfiles/tmux.conf  $HOME/.tmux.conf
-ln -s ./dotfiles/vim.d      $HOME/.vim/
+ln -s $HOME/.my/dotfiles/bashrc     $HOME/.bashrc
+ln -s $HOME/.my/dotfiles/vimrc      $HOME/.vimrc
+ln -s $HOME/.my/dotfiles/tmux.conf  $HOME/.tmux.conf
+ln -s $HOME/.my/dotfiles/vim.d/     $HOME/.vim
